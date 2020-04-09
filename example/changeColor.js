7aebdc474f65f26eab0e7b3d2cd0aa4a288e6124
EZC404 <57637415+EZC404@users.noreply.github.com>
2020-04-09 09:35:03 +0200

    Update changeColor.js

--- example/changeColor.js
+++ example/changeColor.js
@@ -3,4 +3,4 @@ const Controller = require("./MagicLedControll.js");
 var c = new Controller("192.168.137.152",5577);
 
 c.sendCommand(c.setOn(true));
-c.sendCommand(setRGB(255,0,0));
+c.sendCommand(c.setRGB(255,0,0));
